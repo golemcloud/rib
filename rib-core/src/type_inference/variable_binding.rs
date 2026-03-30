94100899f51dee03123cf2ce6a175ee5e80adfbd
Afsal Thaj <afsal.taj06@gmail.com>
2026-03-30 16:05:37 +1100

    move to arena

--- rib-core/src/type_inference/variable_binding.rs
+++ rib-core/src/type_inference/variable_binding.rs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-use crate::{Expr, MatchArm, MatchIdentifier, VariableId};
+use crate::{MatchIdentifier, VariableId};
 use std::collections::HashMap;
 
 use crate::expr_arena::{
