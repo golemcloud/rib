2bc43221720d8122d3e82ceab26ee24a3a4eedca
afsalthaj <afsalthaj@users.noreply.github.com>
2026-04-14 23:22:53 +0000

    deploy: 37056df2800fa4f1990ef25c5dbcaf4dd742e42a

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-32670914.js',
+                path_to_root + 'searchindex-1d3f980a.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
